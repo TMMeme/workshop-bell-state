ccc1c519d753d32927837f0fcdcfea701d6abe63
TMMeme <t.matsubara@bluememe.jp>
2021-07-16 11:24:14 +0900

    Create orquestra_soulution_demo_demo.py

---
+++ steps/orquestra_soulution_demo_demo.py
@@ -0,0 +1,9 @@
+from qiskit import ClassicalRegister, QuantumRegister, QuantumCircuit
+from zquantum.core import circuits
+from zquantum.core.utils import create_object
+
+from zquantum.core.bitstring_distribution import save_bitstring_distribution
+
+import yaml
+
+def main 
