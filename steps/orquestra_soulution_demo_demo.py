841a6185a9ab7b26a43b73785b46f0a6d74e9906
TMMeme <t.matsubara@bluememe.jp>
2021-07-16 11:33:08 +0900

    Update orquestra_soulution_demo_demo.py

--- steps/orquestra_soulution_demo_demo.py
+++ steps/orquestra_soulution_demo_demo.py
@@ -6,4 +6,22 @@ from zquantum.core.bitstring_distribution import save_bitstring_distribution
 
 import yaml
 
-def main 
+def main(backend_specs):
+    circuit=QuantumCircuit(2,2)
+    circuit.h(0)
+    circuit.cx(0,1)
+
+    zap_circuit=circuits.import_from_qiskit(circuit)
+
+    if isinstance(backend_specs, str):
+        backend_specs_dict=yaml.load(backend_specs, Loader=yaml.SafeLoader)
+    else:
+        backend_specs_dict=backend_specs
+    backend=create_object(backend_specs_dict)
+
+    distribution=backend.get_bitstring_distribution(zap_circuit)
+
+    save_bitstring_distribution(distribution, "output-distribution.json")
+
+
+
