a482a8e608f6db566e01002d176c889b32a43f70
TMMeme <t.matsubara@bluememe.jp>
2021-07-21 10:43:15 +0900

    Update orquestra_solution_demo_demo_demo.py

--- steps/orquestra_solution_demo_demo_demo.py
+++ steps/orquestra_solution_demo_demo_demo.py
@@ -1,4 +1,5 @@
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, circuit
+from yaml.loader import SafeLoader
 from zquantum.core import circuits
 from zquantum.core.utils import create_object
 from zquantum.core.bitstring_distribution import save_bitstring_distribution
@@ -14,4 +15,14 @@ def main(backend_specs):
     circuit.cnot(q[0], q[2])
 
     zap_circuit=circuits.import_from_qiskit(circuit)
+
+    if isinstance(backend_specs, str):
+        backend_specs_dict=yaml.load(backend_specs, Loader=yaml.SafeLoader)
+    else:
+        backend_specs_dict=backend_specs
+    backend=create_object(backend_specs_dict)
+
+
+    distribution=get_bitstring_distribution(backend)
+    save_bitstring_distribution(distribution, "output-distribution.json")
     
