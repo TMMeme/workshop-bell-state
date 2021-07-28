5d6c21d593345e5036853b43eb4d204802b41d7a
TMMeme <t.matsubara@bluememe.jp>
2021-07-28 10:40:17 +0900

    Update orquestra_solution_demo_demo.py

--- steps/orquestra_solution_demo_demo.py
+++ steps/orquestra_solution_demo_demo.py
@@ -1,4 +1,11 @@
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 from zquantum.core import circuits
-from zquantum.core.utils import create_circuit
+from zquantum.core.utils import create_object
 from zquantum.core.bitstring_distribution
+import yaml
+
+def main(backend_specs):
+    n=3
+    q=QuantumRegister(n)
+    c=ClassicalRegister(n)
+    circuit=QuantumCircuit(q, c)
